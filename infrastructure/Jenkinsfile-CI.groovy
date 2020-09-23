e75841f02426f0813d347a7b9cab175e3ef8f331
Alex Lanz <alex@krumer.it>
2020-09-23 11:41:43 +0200

    CI pipelines

--- infrastructure/Jenkinsfile-CI.groovy
+++ infrastructure/Jenkinsfile-CI.groovy
@@ -1,15 +1,27 @@
 pipeline {
-    agent {
-        dockerfile {
-            filename 'infrastructure/docker/Dockerfile'
-            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
-        }
+    agent any
+
+    environment {
+        DOCKER_IMAGE = 'odh-mentor-otp'
+        DOCKER_TAG = "$BUILD_NUMBER"
     }
 
     stages {
-        stage('Test') {
+        stage('Configure') {
             steps {
-                sh 'mvn -B -U clean test'
+                sh """
+                    rm -f .env
+                    cp .env.example .env
+                    echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
+                    echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
+                """
+            }
+        }
+        stage('Build') {
+            steps {
+                sh '''
+                    docker-compose --no-ansi -f infrastructure/docker-compose.build.yml build --pull
+                '''
             }
         }
     }
