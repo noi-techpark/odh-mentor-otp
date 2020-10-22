02996ccf250fcc9421c3ac7ffb0d4194c3a4c64e
bertolla <p.bertolla@noi.bz.it>
2020-10-22 15:18:30 +0200

    add missing env vars

--- infrastructure/Jenkinsfile-Test-Execute.groovy
+++ infrastructure/Jenkinsfile-Test-Execute.groovy
@@ -43,6 +43,10 @@ pipeline {
 
                     echo 'OTP_RR_BRANCH=${OTP_RR_BRANCH}' >> .env
                     echo 'OTP_UI_BRANCH=${OTP_UI_BRANCH}' >> .env
+
+                    echo 'API_HOST=${API_HOST}' >> .env
+                    echo 'API_PORT=${API_PORT}' >> .env
+                    echo 'API_PATH=${API_PATH}' >> .env
                 """
             }
         }
