7174cab57922199a8fe1e5dfb67d3122bc513981
Valerio Capogna <valerio.capogna@fornacestudio.com>
2021-04-13 18:11:28 +0200

    change build destination copy bash script

--- journey/app/otp-react-redux/cpbuild.sh
+++ journey/app/otp-react-redux/cpbuild.sh
@@ -1,5 +1,5 @@
 echo 'rebuilding..'
 yarn prepublish
 echo 'copying built js..'
-cp -r ./build ~/git/trimet-mod-otp/node_modules/otp-react-redux
+cp -r ./build ../node_modules/otp-react-redux
 echo 'done'
