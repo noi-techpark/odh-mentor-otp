32a8f9245196a25d18def63760d79b752e1a3bca
Valerio Capogna <valerio.capogna@fornacestudio.com>
2021-04-14 12:18:35 +0200

    refactor wip

--- journey/app/webpack.config.js
+++ journey/app/webpack.config.js
@@ -55,6 +55,10 @@ module.exports = async env => {
             'css-loader',
             'sass-loader'
           ]
+        },
+        {
+          test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+          loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
         }
       ]
     },
