d30da51c97e8d1166aaabf14cffecf3125b8e1b7
Valerio Capogna <valerio.capogna@fornacestudio.com>
2021-04-14 15:55:40 +0200

    wip refactoring

--- journey/app/lib/otp-react-redux/__tests__/test-utils/global-setup.js
+++
@@ -1,7 +0,0 @@
-/**
- * Performs setup of the test environment outside of the browser. This is where
- * items such as environment variables can be set.
- */
-module.exports = async () => {
-  process.env.TZ = 'America/Los_Angeles'
-}
