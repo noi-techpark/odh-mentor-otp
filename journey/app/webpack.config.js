d324702a5442cde4b4b69137b53241e8b4c83ac6
Valerio Capogna <valerio.capogna@fornacestudio.com>
2021-05-17 11:51:47 +0200

    bring all style in lib

--- journey/app/webpack.config.js
+++ journey/app/webpack.config.js
@@ -18,7 +18,6 @@ const TerserPlugin = require('terser-webpack-plugin')
  */
 module.exports = async env => {
   // Gather the CSS, HTML, YAML, and JS override files.
-  const CUSTOM_CSS = env && env.CUSTOM_CSS || './lib/style.scss'
   const HTML_FILE = env && env.HTML_FILE || 'lib/index.tpl.html'
   const YAML_CONFIG = env && env.YAML_CONFIG || './config.yml'
   // resolve the custom js file. If it is present, copy the file to a
@@ -33,8 +32,7 @@ module.exports = async env => {
   }
   return {
     entry: [
-      './lib/main.js',
-      CUSTOM_CSS
+      './lib/main.js'
     ],
     module: {
       rules: [
