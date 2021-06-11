c199d98482b559780f4b41d8e06d457aba332818
stefanocudini <stefano.cudini@gmail.com>
2021-06-11 11:33:25 +0200

    icons parking

--- journey/app/lib/components/map/parking-overlay.js
+++ journey/app/lib/components/map/parking-overlay.js
@@ -9,7 +9,11 @@ import SetFromToButtons from './set-from-to'
 import { setLocation } from '../../actions/map'
 import { parkingLocationsQuery } from '../../actions/parking'
 
-const parkingIcon = '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120.09 120.1"><defs><style>.cls-1{fill:#59ad46;}.cls-2{fill:#fff;}.cls-3{fill:#5c5d5f;}</style></defs><title>zipcar-icon</title><path class="cls-1" d="M246.37,396.78a60,60,0,1,1,60,60,60.05,60.05,0,0,1-60-60" transform="translate(-246.37 -336.74)"/><path class="cls-2" d="M363.6,418.66q0.47-1.28.9-2.58H314.16l2.46-3.15h34.87a1.27,1.27,0,1,0,0-2.53H318.6l2.42-3.09h17.74a1.31,1.31,0,0,0,0-2.58H291.69l28.85-37.59H273.06v10.27h25.28l-26.48,34.34-5.45,6.9h21a12,12,0,0,1,22.29,0H363.6" transform="translate(-246.37 -336.74)"/><path class="cls-3" d="M307.84,423.3a9.27,9.27,0,1,1-9.27-9.27,9.27,9.27,0,0,1,9.27,9.27" transform="translate(-246.37 -336.74)"/></svg>'
+//const parkingIcon = '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120.09 120.1"><defs><style>.cls-1{fill:#59ad46;}.cls-2{fill:#fff;}.cls-3{fill:#5c5d5f;}</style></defs><title>zipcar-icon</title><path class="cls-1" d="M246.37,396.78a60,60,0,1,1,60,60,60.05,60.05,0,0,1-60-60" transform="translate(-246.37 -336.74)"/><path class="cls-2" d="M363.6,418.66q0.47-1.28.9-2.58H314.16l2.46-3.15h34.87a1.27,1.27,0,1,0,0-2.53H318.6l2.42-3.09h17.74a1.31,1.31,0,0,0,0-2.58H291.69l28.85-37.59H273.06v10.27h25.28l-26.48,34.34-5.45,6.9h21a12,12,0,0,1,22.29,0H363.6" transform="translate(-246.37 -336.74)"/><path class="cls-3" d="M307.84,423.3a9.27,9.27,0,1,1-9.27-9.27,9.27,9.27,0,0,1,9.27,9.27" transform="translate(-246.37 -336.74)"/></svg>'
+
+import MarkerStopStation from "../../otp-ui/icons/modern/MarkerStopStation";
+import ReactDOMServer from "react-dom/server";
+
 
 class ParkingOverlay extends MapLayer {
   static propTypes = {
@@ -65,12 +69,22 @@ class ParkingOverlay extends MapLayer {
     const { locations, t } = this.props
     if (!locations || locations.length === 0) return <FeatureGroup />
 
-    const markerIcon = divIcon({
+    /*const markerIcon = divIcon({
       iconSize: [24, 24],
       popupAnchor: [0, -12],
       html: parkingIcon,
       className: ''
-    })
+    })*/
+
+    const markerIcon = divIcon({
+      iconSize: [20, 20],
+      popupAnchor: [0, -10],
+      html: ReactDOMServer.renderToStaticMarkup(
+        <MarkerStopStation width={15} height={15} />
+      ),
+      className: ""
+    });;
+
 
     const bulletIconStyle = {
       color: 'gray',
