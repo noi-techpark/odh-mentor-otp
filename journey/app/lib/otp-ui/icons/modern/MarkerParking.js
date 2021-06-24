ed2753b0b4cc9addede64a6f00615ad442e41070
Valerio Capogna <valerio.capogna@fornacestudio.com>
2021-06-24 17:55:33 +0200

    add new icons and fix the first modern icon created

--- journey/app/lib/otp-ui/icons/modern/MarkerParking.js
+++ journey/app/lib/otp-ui/icons/modern/MarkerParking.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const MarkerParking = ({ title, width = 54, height = 62, ...props }) => (
+const MarkerParking = ({ title, width = 54, height = 62, markerColor = '#b0d1ef', iconColor = '#5d5d5d', ...props }) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} {...props} viewBox="0 0 54 62">
     <g transform="translate(-783 -514)">
-      <rect id="Rectangle_35" data-name="Rectangle 35" width="54" height="51" rx="10" transform="translate(783 514)" fill="#b0d1ef"/>
-      <text id="P" transform="translate(811 554)" fill="#5d5d5d" font-size="37" font-family="HelveticaNeue-Bold, Helvetica Neue" font-weight="700"><tspan x="-12.34" y="0">P</tspan></text>
-      <path id="Path_19" data-name="Path 19" d="M798.4,1802.6l10.96,10.96,10.96-10.96Z" transform="translate(0 -1237.602)" fill="#b0d1ef"/>
+      <rect id="Rectangle_35" data-name="Rectangle 35" width="54" height="51" rx="10" transform="translate(783 514)" fill={markerColor}/>
+      <text id="P" transform="translate(811 554)" fill={iconColor} font-size="37" font-family="HelveticaNeue-Bold, Helvetica Neue" font-weight="700"><tspan x="-12.34" y="0">P</tspan></text>
+      <path id="Path_19" data-name="Path 19" d="M798.4,1802.6l10.96,10.96,10.96-10.96Z" transform="translate(0 -1237.602)" fill={markerColor}/>
     </g>
   </svg>
 );
