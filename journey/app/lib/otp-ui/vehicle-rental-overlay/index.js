5a91c64183ba5a164bb845eee9eab1f1ad510fbf
Federico Bertolini <f.bertolini@riseup.net>
2021-07-21 17:54:17 +0200

    aggregate floatingBike to near virtual station

--- journey/app/lib/otp-ui/vehicle-rental-overlay/index.js
+++ journey/app/lib/otp-ui/vehicle-rental-overlay/index.js
@@ -248,6 +248,48 @@ class VehicleRentalOverlay extends MapLayer {
       return <FeatureGroup />;
     }
 
+    const deg2rad = (deg) => {
+      return deg * (Math.PI/180)
+    }
+    const getDistanceFromLatLonInMeters = (lat1, lon1, lat2, lon2) => {
+      
+      var R = 6371 * 1000; // Radius of the earth in meters
+      var dLat = deg2rad(lat2-lat1);  // deg2rad below
+      var dLon = deg2rad(lon2-lon1); 
+      var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ; 
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      var d = R * c; // Distance in meters
+      return d;
+    }
+
+    for(var station of filteredStations){
+      if(station.isFloatingBike){
+        let nearest = null;
+        let lastDistance = null;
+        for(var i = 0; i <  filteredStations.length; i++){
+          const mstation = filteredStations[i];          
+          if(mstation.isFloatingBike === false 
+            && mstation.networks[0] == station.networks[0]
+            ){
+              const distance = getDistanceFromLatLonInMeters(station.y, station.x, mstation.y, mstation.x);
+              if (lastDistance == null || distance < lastDistance){
+                nearest = i;
+                lastDistance = distance;        
+              }
+                  
+          }
+        }
+        if(nearest){
+          filteredStations[nearest].bikesAvailable += 1;
+        }
+
+      }
+    }
+
     return (
       <FeatureGroup>{filteredStations.map(this.renderStation)}</FeatureGroup>
     );
