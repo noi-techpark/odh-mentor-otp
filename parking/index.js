36f1c17de8b85a9af202dda248a2e74c68fd3591
stefanocudini <stefano.cudini@gmail.com>
2021-07-21 17:40:59 +0200

    parking sensors

--- parking/index.js
+++ parking/index.js
@@ -161,6 +161,54 @@ app.get('/parking/sensors.json', cors(corsOptions), function (req, res) {
     });
 });
 
+app.get('/parking/all.json', cors(corsOptions), function (req, res) {
+    var parkingAll = [];
+    if(stationsReceived){
+        for(var i = 0; i < stationsReceived.length; i++){
+            var station = stationsReceived[i];
+            if(station.sactive && station.scoordinate && station.smetadata){
+                parkingAll.push({
+                    type: 'station',
+                    station_id: station.scode,
+                    name: station.sname,
+                    lat: station.scoordinate.y,
+                    lon: station.scoordinate.x,
+                    address: station.smetadata.mainaddress,
+                    city: station.smetadata.municipality,
+                    capacity: station.smetadata.capacity || 0,
+                    free: station.mvalue || 0
+                })
+            }
+        }
+    }
+
+    if(sensorsReceived){
+        for(var i = 0; i < sensorsReceived.length; i++){
+            var sensor = sensorsReceived[i];
+            if(sensor.sactive && sensor.scoordinate && sensor.smetadata){
+                parkingAll.push({
+                    type: 'sensor',
+                    sensor_id: sensor.scode,
+                    name: sensor.sname,
+                    lat: sensor.scoordinate.y,
+                    lon: sensor.scoordinate.x,
+                    address: sensor.smetadata.group,
+                    city: sensor.smetadata.municipality,
+                    free: sensor.mvalue === 1 ? false : true
+                })
+            }
+        }
+    }
+    res.json({
+        last_updated: lastUpdate,
+        ttl: 0,
+        version: "1.0",
+        data: {
+            stations: parkingAll
+       }
+    });
+});
+
 
 var server = app.listen(config.server.port, function () {
    console.log("Listening on port ", config.server.port);
