917bb0c884ac0caa9ec45fa6471193805cb57b8a
stefanocudini <stefano.cudini@gmail.com>
2021-08-16 11:59:57 +0200

    simplify credits page

--- journey/app/lib/components/app/modal-credits.js
+++ journey/app/lib/components/app/modal-credits.js
@@ -2,47 +2,33 @@ import React, { Component } from 'react'
 import { Modal } from 'react-bootstrap'
 import { withNamespaces } from "react-i18next"
 
-import logoPlaceholder from '../../images/credits/logo-placeholder.png'
+import imageLogo from '../../images/credits/logo-placeholder.png'
+
+//ADD MORE LOGO IMAGES HERE:
+//import imageLogo2 from '../../images/credits/logo-2.png'
+//import imageLogo3 from '../../images/credits/logo-3.png'
 
 class ModalCredits extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      showCredits: false
-    }
-  }
-
   render () {
     const { t, show} = this.props
-
     return (
         <Modal bsSize="large" show={show} onHide={ () => this.props.onClose() }>
-          <Modal.Header closeButton>
-            <Modal.Title>{t('credits_title')}</Modal.Title>
-          </Modal.Header>
+          <Modal.Header closeButton><Modal.Title>{t('credits_title')}</Modal.Title></Modal.Header>
           <Modal.Body>
+
+          {/* START CONTENTS of CREDITS */}
+
             <h6 className="credits-title">{t('credits_title_developed')}</h6>
 
             <ul className="credits-list">
               <li>
                 <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
+                  <img src={imageLogo} />
                 </a>
               </li>
               <li>
                 <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
+                  <img src={imageLogo} />
                 </a>
               </li>
             </ul>
@@ -52,27 +38,7 @@ class ModalCredits extends Component {
             <ul className="credits-list">
               <li>
                 <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
+                  <img src={imageLogo} />
                 </a>
               </li>
             </ul>
@@ -82,39 +48,21 @@ class ModalCredits extends Component {
             <ul className="credits-list">
               <li>
                 <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
+                  <img src={imageLogo} />
                 </a>
               </li>
               <li>
                 <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
-                </a>
-              </li>
-              <li>
-                <a href="#" target="_blank">
-                  <img src={logoPlaceholder} alt="Company Name"/>
+                  <img src={imageLogo} />
                 </a>
               </li>
             </ul>
+
+            {/* END CONTENTS of CREDITS */}
+
           </Modal.Body>
         </Modal>
     )
   }
 }
-
 export default withNamespaces()(ModalCredits)
