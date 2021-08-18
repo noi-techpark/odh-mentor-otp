eaf02c5fb477c9d5ab0f7f8f14c0279b0d626d49
stefanocudini <stefano.cudini@gmail.com>
2021-08-18 14:06:03 +0200

    fix more styles of markers

--- journey/app/lib/otp-ui/icons/modern/MarkerStopStation.js
+++ journey/app/lib/otp-ui/icons/modern/MarkerStopStation.js
@@ -3,7 +3,7 @@ import React from "react";
 const MarkerStopStation = ({ title, iconColor = '#fff', markerColor = '#fff', ...props }) => (
   <svg xmlns="http://www.w3.org/2000/svg" {...props} viewBox="0 0 19 19">
     {title ? <title>{title}</title> : null}
-    <g fill={ iconColor } stroke={markerColor} strokeWidth="4">
+    <g fill={iconColor} stroke={markerColor} strokeWidth="4">
       <circle cx="9.5" cy="9.5" r="9.5" stroke="none"/>
       <circle cx="9.5" cy="9.5" r="7.5" fill="none"/>
     </g>
