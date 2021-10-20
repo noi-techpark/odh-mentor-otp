bee5bf1cea0223cbeaebaa973d3ed17613275838
stefanocudini <stefano.cudini@gmail.com>
2021-10-20 17:02:30 +0200

    fix vehicles

--- journey/app/lib/otp-ui/icons/modern/MarkerDrtStop.js
+++ journey/app/lib/otp-ui/icons/modern/MarkerDrtStop.js
@@ -3,8 +3,9 @@ import React from "react";
 const MarkerDrtStop = ({ title, iconColor = '#fff', markerColor = '#fff', ...props }) => (
   <svg xmlns="http://www.w3.org/2000/svg" {...props} viewBox="-2 -2 23 23">
     {title ? <title>{title}</title> : null}
-    <g fill={iconColor} stroke={markerColor} strokeWidth="3">
-      <rect width="14" height="14" />
+    <g fill={iconColor} stroke={markerColor} strokeWidth="4">
+
+    <circle cx="9.5" cy="9.5" r="9.5" fill="{iconColor}"/>
     </g>
   </svg>
 );
