30ae50a2b6e5ab54f5b4b07978e59989e57dec50
stefanocudini <stefano.cudini@gmail.com>
2021-11-22 16:10:35 +0100

    link tool

--- infrastructure/docker/otp/docker-entrypoint.sh
+++ infrastructure/docker/otp/docker-entrypoint.sh
@@ -10,8 +10,13 @@ if [ "${DOWNLOAD_DATA}" = "True" ]; then
 	#TODO move to file download-data.sh
 	#
 	#
-	# online tool selection: http://dwtkns.com/srtm/	90m
-	# # online tool selection: http://dwtkns.com/srtm30m/	30m
+	# online tool selection:
+	#
+	# free 90m: http://dwtkns.com/srtm
+	#
+	# SRTM 30m: http://dwtkns.com/srtm30m/
+	# require login https://urs.earthdata.nasa.gov/
+	#
 
 	srtmurl="http://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/TIFF/srtm_39_03.zip"
 	srtmzip=/data/srtm_39_03.zip
