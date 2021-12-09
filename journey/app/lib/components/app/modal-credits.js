fdf4f3106411dafa688cd87890eb6ce0a3fbe304
rcavaliere <r.cavaliere@noi.bz.it>
2021-12-09 08:51:17 +0100

    Insert landing page reference

--- journey/app/lib/components/app/modal-credits.js
+++ journey/app/lib/components/app/modal-credits.js
@@ -31,6 +31,7 @@ class ModalCredits extends Component {
           <Modal.Header closeButton><Modal.Title>{t('credits_title')}</Modal.Title></Modal.Header>
           <Modal.Body>
 
+          <a href="https://www.gemeinde.meran.bz.it/it/mobility_meran_eu" target="_blank">
           <img src={interregLogo} />
 
           {/* START CONTENTS of CREDITS */}
