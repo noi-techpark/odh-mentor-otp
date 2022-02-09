9d25620a02ae99fee720d70b62c3d47fb3a7d3a7
stefanocudini <stefano.cudini@gmail.com>
2022-02-09 19:08:24 +0100

    traffic

--- traffic/index.js
+++ traffic/index.js
@@ -140,7 +140,7 @@ app.get('/traffic/stations.json', cors(corsOptions),  function (req, res) {
 app.get('/traffic/linkstations.json', cors(corsOptions), async function (req, res) {
 //source: https://mobility.api.opendatahub.bz.it/v2/tree/LinkStation/*/latest?limit=-1&distinct=true&select=tmeasurements&where=sactive.eq.true,or(and(tname.eq.%22Bluetooth%20Elapsed%20time%20%5C(test%5C)%22))
 
-    console.log('[TRAFFIC] request /traffic/linkstations.json')
+    console.log('[traffic] request /traffic/linkstations.json')
     var linkstations = [];
     const stationsById = {};
 
