8079c3f70a785ae71ac3fbcf439d5580bf096916
RudiThoeni <r.thoeni@noi.bz.it>
2022-12-13 11:28:29 +0100

    updating authserver url

--- vms/token.sh
+++ vms/token.sh
@@ -23,7 +23,7 @@ CLIENT_ID=odh-generic-client
 
 #PAYLOAD="grant_type=password&username=$API_USER&password=$API_PASS&client_id=$CLIENTID"
 
-curl -X POST -L "https://auth.opendatahub.bz.it/auth/realms/noi/protocol/openid-connect/token" \
+curl -X POST -L "https://auth.opendatahub.com/auth/realms/noi/protocol/openid-connect/token" \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'grant_type=password' \
 --data-urlencode "username=${API_USER}" \
