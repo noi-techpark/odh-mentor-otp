41a32e79a992018a2e7cefd29d3d0610d12de6ea
Leonard Ehrenfried <mail@leonard.io>
2024-01-29 17:42:52 +0100

    Remove elevation data again

--- build-graph.sh
+++ build-graph.sh
@@ -9,11 +9,6 @@ NORTH_EAST_URL=https://download.geofabrik.de/europe/italy/nord-est-latest.osm.pb
 NORTH_EAST_PBF=data/italy-nord-est.osm.pbf
 SOUTH_TYROL_PBF=data/south-tyrol.osm.pbf
 
-# elevation
-SRTM_URL="http://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/TIFF/srtm_39_03.zip"
-SRTM_ZIP=data/srtm_39_03.zip
-SRTM_TIFF=data/srtm_39_03.tif
-
 # OTP
 OTP_IMAGE=docker.io/opentripplanner/opentripplanner:2.5.0_2024-01-19T14-50
 
@@ -30,16 +25,6 @@ fi
 
 # cut out South Tyrol from the large South East Italy extract
 osmium extract ${NORTH_EAST_PBF} --polygon south-tyrol.geojson -o ${SOUTH_TYROL_PBF} --overwrite
-
-# download elevation data
-if [ ! -f "${SRTM_ZIP}" ]; then
-  wget --progress=bar:force:noscroll ${SRTM_URL} -O ${SRTM_ZIP}
-fi
-
-unzip -o ${SRTM_ZIP} -d data
-# fix srtm data with gdal
-gdal_edit.py -unsetnodata ${SRTM_TIFF}
-
 
 # actually do graph build
 docker run \
