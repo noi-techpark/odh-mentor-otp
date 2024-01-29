2c97d80200ba54b5fbadfb82ed655e28508cb8f5
Leonard Ehrenfried <mail@leonard.io>
2024-01-29 17:11:24 +0100

    Clean up script, don't use --no-clobber

--- build-graph.sh
+++ build-graph.sh
@@ -4,6 +4,12 @@
 #
 # SPDX-License-Identifier: CC0-1.0
 
+NORTH_EAST_URL=https://download.geofabrik.de/europe/italy/nord-est-latest.osm.pbf
+NORTH_EAST_PBF=data/italy-nord-est.osm.pbf
+SOUTH_TYROL_PBF=data/south-tyrol.osm.pbf
+
+OTP_IMAGE=docker.io/opentripplanner/opentripplanner:2.5.0_2024-01-19T14-50
+
 # when on github actions then install the required tools
 if [ -n "${CI+isset}" ]; then
   sudo apt-get -q install osmium-tool wget
@@ -11,14 +17,14 @@ fi
 
 mkdir -p data
 
-NORTH_EASTH_PBF=data/italy-nord-est.osm.pbf
-SOUTH_TYROL_PBF=data/south-tyrol.osm.pbf
+if [ ! -f "${NORTH_EAST_PBF}" ]; then
+  wget --progress=bar:force:noscroll ${NORTH_EAST_URL} -O ${NORTH_EAST_PBF}
+fi
 
-OTP_IMAGE=docker.io/opentripplanner/opentripplanner:2.5.0_2024-01-19T14-50
+# cut out South Tyrol from the large South East Italy extract
+osmium extract ${NORTH_EAST_PBF} --polygon south-tyrol.geojson -o ${SOUTH_TYROL_PBF} --overwrite
 
-wget --progress=bar:force:noscroll --no-clobber https://download.geofabrik.de/europe/italy/nord-est-latest.osm.pbf -O ${NORTH_EASTH_PBF}
-osmium extract ${NORTH_EASTH_PBF} --polygon south-tyrol.geojson -o ${SOUTH_TYROL_PBF} --overwrite
-
+# actually do graph build
 docker run \
   -v .:/var/opentripplanner/:z \
   --rm \
