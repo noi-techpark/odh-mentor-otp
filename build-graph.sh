30a9342dc998001975053545689f77d2a2dc3602
Leonard Ehrenfried <mail@leonard.io>
2024-01-29 17:21:19 +0100

    Add elevation

--- build-graph.sh
+++ build-graph.sh
@@ -4,10 +4,16 @@
 #
 # SPDX-License-Identifier: CC0-1.0
 
+# OSM
 NORTH_EAST_URL=https://download.geofabrik.de/europe/italy/nord-est-latest.osm.pbf
 NORTH_EAST_PBF=data/italy-nord-est.osm.pbf
 SOUTH_TYROL_PBF=data/south-tyrol.osm.pbf
 
+# elevation
+SRTM_URL="http://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/TIFF/srtm_39_03.zip"
+SRTM_ZIP=data/srtm_39_03.zip
+
+# OTP
 OTP_IMAGE=docker.io/opentripplanner/opentripplanner:2.5.0_2024-01-19T14-50
 
 # when on github actions then install the required tools
@@ -23,6 +29,13 @@ fi
 
 # cut out South Tyrol from the large South East Italy extract
 osmium extract ${NORTH_EAST_PBF} --polygon south-tyrol.geojson -o ${SOUTH_TYROL_PBF} --overwrite
+
+if [ ! -f "${SRTM_ZIP}" ]; then
+  wget --progress=bar:force:noscroll ${SRTM_URL} -O ${SRTM_ZIP}
+fi
+
+unzip -o ${SRTM_ZIP} -d data
+
 
 # actually do graph build
 docker run \
