91a7d26b8f4603644be8ac0772c8a258bad59f90
Leonard Ehrenfried <mail@leonard.io>
2024-02-05 12:29:15 +0100

    Add elecation data

--- build-graph.sh
+++ build-graph.sh
@@ -4,10 +4,15 @@
 #
 # SPDX-License-Identifier: CC0-1.0
 
+WGET="wget --progress=bar:force:noscroll"
+
 # OSM
 NORTH_EAST_URL=https://download.geofabrik.de/europe/italy/nord-est-latest.osm.pbf
 NORTH_EAST_PBF=data/italy-nord-est.osm.pbf
 SOUTH_TYROL_PBF=data/south-tyrol.osm.pbf
+# elevation
+ELEVATION_URL=https://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/TIFF/srtm_39_03.zip
+ELEVATION_ZIP=data/srtm_39_03.zip
 
 # OTP
 OTP_IMAGE=docker.io/opentripplanner/opentripplanner:2.5.0_2024-01-19T14-50
@@ -20,11 +25,17 @@ fi
 mkdir -p data
 
 if [ ! -f "${NORTH_EAST_PBF}" ]; then
-  wget --progress=bar:force:noscroll ${NORTH_EAST_URL} -O ${NORTH_EAST_PBF}
+  ${WGET} ${NORTH_EAST_URL} -O ${NORTH_EAST_PBF}
 fi
 
 # cut out South Tyrol from the large North East Italy extract
 osmium extract ${NORTH_EAST_PBF} --polygon south-tyrol.geojson -o ${SOUTH_TYROL_PBF} --overwrite
+
+if [ ! -f "${ELEVATION_ZIP}" ]; then
+  ${WGET} ${ELEVATION_URL} -O ${ELEVATION_ZIP}
+  unzip -o ${ELEVATION_ZIP} -d data
+fi
+
 
 # actually do graph build
 docker run \
