e60ca449d0cc55d36a13107639cd80f6f6ad003b
Simon Dalvai <info@simondalvai.org>
2024-03-28 16:09:28 +0100

    amarillo: wait for 5 minutes

--- infrastructure/docker/amarillo/docker-entrypoint.sh
+++ infrastructure/docker/amarillo/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # amarillo needs started graphopper, so wait some time to let it startup
 echo "sleeping to wait for graphhopper startup..."
-sleep 60;
+sleep 300;
 echo "sleeping over. graphhopper are you there?"
 bash /app/prestart.sh
 uvicorn amarillo.main:app --host 0.0.0.0 --port 80;
