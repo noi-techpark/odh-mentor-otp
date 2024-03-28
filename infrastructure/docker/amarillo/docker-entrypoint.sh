2983da6716a51abddcedecb1e91d77dc7ab40d1e
Simon Dalvai <info@simondalvai.org>
2024-03-28 14:05:18 +0100

    infra: let amarillo sleep for 60s to wait for graphhopper

---
+++ infrastructure/docker/amarillo/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# amarillo needs started graphopper, so wait some time to let it startup
+echo "sleeping to wait for graphhopper startup..."
+sleep 60;
+echo "sleeping over. graphhopper are you there?"
+bash /app/prestart.sh
+uvicorn amarillo.main:app --host 0.0.0.0 --port 80;
