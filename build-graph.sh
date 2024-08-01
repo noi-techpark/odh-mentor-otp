6f2a827db82b0512f581646f3d75aae7f96d2f97
Leonard Ehrenfried <mail@leonard.io>
2024-08-01 16:53:35 +0200

    Configure NeTEx parking data

--- build-graph.sh
+++ build-graph.sh
@@ -15,10 +15,14 @@ SOUTH_TYROL_PBF=data/south-tyrol.osm.pbf
 # elevation
 ELEVATION_URL=https://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/TIFF/srtm_39_03.zip
 ELEVATION_ZIP=data/srtm_39_03.zip
+# parking
+PARKING_NETEX_URL=https://transmodel.api.opendatahub.com/netex/parking
+PARKING_NETEX_XML=data/shared-data.xml
+PARKING_NETEX_ZIP=data/parking-netex.zip
 
 # when on github actions then install the required tools
 if [ -n "${CI+isset}" ]; then
-  sudo apt-get -qq install osmium-tool pyosmium wget
+  sudo apt-get -qq install osmium-tool pyosmium wget zip
 fi
 
 mkdir -p data
@@ -38,6 +42,12 @@ if [ ! -f "${ELEVATION_ZIP}" ]; then
   ${WGET} ${ELEVATION_URL} -O ${ELEVATION_ZIP}
   unzip -o ${ELEVATION_ZIP} -d data
 fi
+
+# download parking data and put it into a zip
+rm -f ${PARKING_NETEX_XML} ${PARKING_NETEX_ZIP}
+wget ${PARKING_NETEX_URL} -O ${PARKING_NETEX_XML}
+
+zip --junk-paths ${PARKING_NETEX_ZIP} ${PARKING_NETEX_XML}
 
 
 # actually do graph build
